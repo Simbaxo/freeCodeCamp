0dae300dc0f2a5c1dcbb380936a9598b82892768
Andres Mena <ballerlikemike23@gmail.com>
2019-10-05 13:22:01 -0400

    Mutations ✅

--- Basic Algorithm Scripting/14-mutations.js
+++ Basic Algorithm Scripting/14-mutations.js
@@ -7,8 +7,61 @@ The arguments ["hello", "hey"] should return false because the string "hello" do
 Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
 */
 
+/* My Solution */
 function mutation(arr) {
-  return arr;
+  // sort arrs by smallest word
+  arr.sort(function(a, b) {
+    return a.length - b.length
+  })
+  // match every letter in first arr to second arr
+  return arr[0].toLowerCase().split('').every(function(x) {
+    if(arr[1].toLowerCase().indexOf(x) === -1) {
+      return false
+    }
+    else {
+      return true
+    }
+  })
 }
 
-mutation(["hello", "hey"]);
+mutation(["hello", "hey"])
+
+/* FCC Answers
+Basic Code Solution
+*/
+
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (var i=0;i<test.length;i++) {
+    if (target.indexOf(test[i]) < 0)
+      return false;
+  }
+  return true;
+ }
+
+/* Code Explanation
+First we make the two strings in the array lowercase. test will hold what we are looking for in target.
+Then we loop through our test characters and if any of them is not found we return false.
+
+If they are all found, the loop will finish without returning anything and we get to return true.
+*/
+
+/* FCC Answers
+Intermediate Code Solution
+*/
+
+function mutation(arr) {
+  return arr[1].toLowerCase()
+    .split('')
+    .every(function(letter) {
+      return arr[0].toLowerCase()
+        .indexOf(letter) != -1;
+    });
+}
+
+/* Code Explanation
+Grab the second string, lowercase and turn it into an array; then make sure every one of its letters is a part of the lowercased first string.
+
+Every will basically give you letter by letter to compare, which we do by using indexOf on the first string. indexOf will give you -1 if the current letter is missing. We check that not to be the case, for if this happens even once every will be false.
+*/
